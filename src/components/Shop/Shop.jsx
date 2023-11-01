af89e9362a4c1b5b254507a56f0ecdb4227ae63c
srs-sieam <srssieam@gmail.com>
2023-11-01 19:07:07 +0600

    get total count of products in database collection from server

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,14 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    // const count = useLoaderData();
+    const { count } = useLoaderData();
+    console.log(count)
 
     useEffect(() => {
         fetch('http://localhost:5000/products')
