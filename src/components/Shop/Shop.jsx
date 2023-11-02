18cfa1b1a3ca88aa33ccdfb1749051d3b8be00e8
srs-sieam <srssieam@gmail.com>
2023-11-02 06:10:15 +0600

    send pagination peremeter to the server using query

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -26,10 +26,10 @@ const Shop = () => {
     console.log(pages)
 
     useEffect(() => {
-        fetch('http://localhost:5000/products')
+        fetch(`http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`)
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, []);
+    }, [currentPage, itemsPerPage]);
 
     useEffect(() => {
         const storedCart = getShoppingCart();
